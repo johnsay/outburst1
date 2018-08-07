9b9d0851ae6af1a5249404d80996837aee8412cd
Damien Delmarle <>
2018-08-07 23:34:04 +0800

    start scene

---
+++ Assets/Outburst/Script/Start/StartScene.cs
@@ -0,0 +1,14 @@
+﻿
+using FoundationFramework;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour
+{
+	private const float LoadDelay = 3;
+	private const string HubScene = "Hub";
+
+	private void Start () 
+	{
+		Timer.Register(LoadDelay, () => SceneLoader.Instance.LoadScene(HubScene));
+	}
+}
