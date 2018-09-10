2df680240d5c824bfd693be4854a6a845071fda8
Damien Delmarle <>
2018-09-10 22:26:56 +0800

    no message

---
+++ Assets/Outburst/Scripts/Hub/GameModeHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeHandler: Singleton<GameModeHandler>
+{
+    public enum GameMode{ Casual, TeamBattle }
+
+    public GameMode CurrentMode = GameMode.Casual;
+
+    public static Action OnModeUpdated;
+
+    public void UpdateMode(GameMode mode)
+    {
+        CurrentMode = mode;
+        if (OnModeUpdated != null) OnModeUpdated();
+    }
+
+}
---
+++ Assets/Outburst/Scripts/Hub/UiModeSelection.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UiModeSelection : MonoBehaviour 
+{
+	#region FIELDS
+
+	[SerializeField] private UiPanelBase _panel;
+	[SerializeField] private Button _casualButton;
+	[SerializeField] private Button _teamButton;
+
+	#endregion
+
+	public void OpenPanel()
+	{
+		_casualButton.interactable = true;
+		_teamButton.interactable = true;
+		_panel.Show();
+	}
+
+	public void HidePanel()
+	{
+		_casualButton.interactable = false;
+		_teamButton.interactable = false;
+		_panel.Hide();
+	}
+
+	public void SelectTeamMode()
+	{
+		HidePanel();
+		GameModeHandler.Instance.UpdateMode(GameModeHandler.GameMode.TeamBattle);
+	}
+
+	public void SelectCasualMode()
+	{
+		HidePanel();
+		GameModeHandler.Instance.UpdateMode(GameModeHandler.GameMode.Casual);
+	}
+
+
+}
